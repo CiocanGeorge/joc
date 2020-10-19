5538996928b8a6f749ed556aac92b4e6a6d3252e
spectral369 <spectral.shadow@yahoo.com>
2020-10-19 14:53:45 +0300

    Fixed player hitbox

--- player.py
+++ player.py
@@ -7,13 +7,13 @@ class Player:
         self.screen = screen
         self.pygame = pygame
         self.rect = pygame.Rect(x, y, 38,38)
-        self.hitbox = (self.rect.x + 16, self.rect.y + 20, 38, 38)
+       # self.hitbox = (self.rect.x, self.rect.y, 60, 60)
     def draw(self):
         #surf = self.pygame.Surface((50 ,50), self.pygame.HWSURFACE | self.pygame.SRCALPHA)
         self.screen.blit(self.image,(self.rect.x,self.rect.y))
        # surf.blit(self.image,(50,50))
-        self.hitbox = (self.rect.x + 16, self.rect.y + 20, 38, 38)
-        self.rect = self.pygame.Rect( self.rect.x, self.rect.y, 38,38)
+        self.hitbox = (self.rect.x , self.rect.y , 64, 62)
+        self.rect = self.pygame.Rect( self.rect.x, self.rect.y, 64,62)
         self.pygame.draw.rect(self.screen, (255,0,0), self.hitbox,2)
     def move(self,speedx,speedy):
         #self.x += speedx
